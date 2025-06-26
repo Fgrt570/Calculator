4d7596e2821f820ba86216f1e48879d4e02fe950
Fgrt570 <neka.kull@mail.ru>
2025-06-26 20:13:19 +0300

    Добавьте файлы проекта.

---
+++ Calcul/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcul
+{
+    public partial class Form1 : Form
+    {
+        double Number1;
+        string Operation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "0";
+            else
+                textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "1";
+            else
+                textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "2";
+            else
+                textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "3";
+            else
+                textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "4";
+            else
+                textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "5";
+            else
+                textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "6";
+            else
+                textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "7";
+            else
+                textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "8";
+            else
+                textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1 != null)
+                textBox1.Text = "9";
+            else
+                textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains(","))
+                textBox1.Text += ",";
+            
+        }
+        private void backspace_Click(object sender, EventArgs e)
+        {
+            int length = textBox1.TextLength - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < length; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+        private void equals1_Click(object sender, EventArgs e)          
+        {
+            double Number2;
+            double Result;
+
+            Number2 = Convert.ToDouble(textBox1.Text);
+            if (Number2 == 0)
+                MessageBox.Show(("Ошибка : Выражение не обнаружено("), "Ошибка");
+            else
+            {
+                if (Operation == "+")
+                {
+                    Result = (Number1 + Number2);
+                    textBox1.Text = Convert.ToString(Result);
+                    Number1 = Result;
+                }
+                if (Operation == "-")
+                {
+                    Result = (Number1 - Number2);
+                    textBox1.Text = Convert.ToString(Result);
+                    Number1 = Result;
+                }
+                if (Operation == "*")
+                {
+
+                    {
+                        Result = (Number1 * Number2);
+                        textBox1.Text = Convert.ToString(Result);
+                        Number1 = Result;
+                    }
+                }
+                if (Operation == "/")
+                {
+                    if (Number2 == 0)
+                        MessageBox.Show(("Ошибка : Нельзя делить на нуль"), "Ошибка");
+                    else
+                    {
+                        Result = (Number1 / Number2);
+                        textBox1.Text = Convert.ToString(Result);
+                        Number1 = Result;
+                    }
+                }
+                if (Operation == "log")
+                {
+                    Result = Math.Log(Number2);
+                    textBox1.Text = Convert.ToString(Result);
+                    Number1 = Result;
+                }
+                if (Operation == "sqrt")
+                {
+                    Result = Math.Sqrt(Number1);
+                    textBox1.Text = Convert.ToString(Result);
+                    Number1 = Result;
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            Number1 = Convert.ToDouble(textBox1.Text);
+            if (Number1 == 0) { MessageBox.Show(("Ошибка : Введите число!"), "Ошибка"); }
+      
+            else
+            {
+                textBox1.Text = "0";
+                Operation = "/";
+            }  
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Number1 = Convert.ToDouble(textBox1.Text);
+            if (Number1 == 0) { MessageBox.Show(("Ошибка : Введите число!"), "Ошибка"); }
+            else
+            {                
+                textBox1.Text = "0";
+                Operation = "*";
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Number1 = Convert.ToDouble(textBox1.Text);
+            if (Number1 == 0) { MessageBox.Show(("Ошибка : Введите число!"), "Ошибка"); }
+            else
+            {
+            textBox1.Text = "0";
+            Operation = "-";
+            }
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Number1 = Convert.ToDouble(textBox1.Text);
+            if (Number1 == 0) { MessageBox.Show(("Ошибка : Введите число!"), "Ошибка"); }
+            else
+            {
+                textBox1.Text = "0";
+                Operation = "+";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Number1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operation = "log";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Number1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operation = "sqrt";
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Form2 newfrm = new Form2();//Form2 -- вторая форма
+            newfrm.Show();
+        }
+    }
+}
---
+++ Calcul/Form2.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Calcul
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button21 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button21
+            // 
+            this.button21.BackColor = System.Drawing.Color.IndianRed;
+            this.button21.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button21.Font = new System.Drawing.Font("Agency FB", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button21.Location = new System.Drawing.Point(269, 12);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(35, 23);
+            this.button21.TabIndex = 23;
+            this.button21.Text = "X";
+            this.button21.UseVisualStyleBackColor = false;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.listBox1.Font = new System.Drawing.Font("Agency FB", 15.75F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "\"X\" - умножение;",
+            "\"/\" - деление;",
+            "\"log\" - вычисление логарифма;",
+            "\"sqrt\" - вычисление корней;",
+            "\"-\" - вычитание;",
+            "\"+\" - сложение;",
+            "\"C\" - удалить все;",
+            "\"CE\" - удалить последнюю ",
+            "цифру;"});
+            this.listBox1.Location = new System.Drawing.Point(12, 213);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(292, 225);
+            this.listBox1.TabIndex = 24;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(316, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button21);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
